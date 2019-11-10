cd2da5412bda4f654f0351ff36e85726a6e6bc74
saraAbdull <alkharashi.sara@gmail.com>
2019-11-10 15:23:15 +0300

    hello strings

---
+++ demo/src/main/java/com/kharashi/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.kharashi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping("/")
+    public String hello() { 
+            return "Hello Client! How are you doing?";
+    }
+	@RequestMapping("/random")
+    public String rand() { 
+            return "Spring boot in easy!";
+    }
+
+}
